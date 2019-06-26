18712a22f0b168e0c8e3c9e4d3adf7df0ab597c8
zc470318528 <46548182+zc470318528@users.noreply.github.com>
2019-06-26 09:16:35 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -3,18 +3,18 @@ NODE_ID = 0
 
 
 # hour,set 0 to disable
-SPEEDTEST = 6
+SPEEDTEST = 240
 CLOUDSAFE = 1
 ANTISSATTACK = 0
 AUTOEXEC = 0
 
-MU_SUFFIX = 'zhaoj.in'
+MU_SUFFIX = 'cloudflare.com'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
 API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
-WEBAPI_URL = 'https://zhaoj.in'
+WEBAPI_URL = 'https://xiaoqiu.online'
 WEBAPI_TOKEN = 'glzjin'
 
 # mudb
