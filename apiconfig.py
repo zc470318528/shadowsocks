3f2eccfd2541d99296952bbfc85ec4bac47f2fe9
zc470318528 <46548182+zc470318528@users.noreply.github.com>
2019-07-05 15:07:47 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -14,7 +14,7 @@ MU_REGEX = '%5m%id.%suffix'
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
 API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
-WEBAPI_URL = 'https://xiaoqiu.online'
+WEBAPI_URL = 'https://murasamefans.com'
 WEBAPI_TOKEN = 'glzjin'
 
 # mudb
